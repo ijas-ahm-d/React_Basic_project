db91e7011ed8eb90b61ec8f51ab1ab69c4b745dc
ijas-ahm-d <ijasahammed2000@gmail.com>
2024-05-04 12:30:23 +0530

    Main Page initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import MyAppBar from "./AppBar";
+import MyDrawer from './Drawer';
+import StudentTable from './StudentTable';
 
-function App() {
+import './App.css';
+import StudentTable from './StudentTable';
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const drawerAction = () => {
+    setOpen(!open)
+  };
+  const students = [];
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
+    <div>
+      <MyAppBar ToggleFunction={drawerAction} />
+      <MyDrawer open={open} drawerAction={drawerAction}/>
+      <StudentTable students={students}/>
     </div>
   );
-}
+};
 
 export default App;
